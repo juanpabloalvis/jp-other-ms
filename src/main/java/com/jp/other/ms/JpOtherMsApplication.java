08d16e30b00be40893a82a4285e92d8f671925ff
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-18 20:04:53 -0500

    Initial app

---
+++ src/main/java/com/jp/other/ms/JpOtherMsApplication.java
@@ -0,0 +1,13 @@
+package com.jp.other.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpOtherMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpOtherMsApplication.class, args);
+	}
+
+}
